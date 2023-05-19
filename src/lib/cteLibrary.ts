78d9eff000b7bcd56dc506dc99a7b1969b283690
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-05-19 17:11:54 -0600

    refactor standards

--- src/lib/cteLibrary.ts
+++ src/lib/cteLibrary.ts
@@ -1,8 +1,13 @@
 export interface CTESkill {
   id: string
   strand: string
-  standards: string[]
+  standards: Standard[]
   performance_skills: string[]
+}
+
+interface Standard {
+  standard: string
+  data: string[]
 }
 
 export const cteLibrary: CTESkill[] = [
@@ -11,9 +16,24 @@ export const cteLibrary: CTESkill[] = [
     strand:
       "Digital Citizenship: Students will understand the basic principles of copyright, AUP, and ethical behavior.",
     standards: [
-      "Demonstrate knowledge of standard copyright rules.",
-      "Identify the use and purpose of acceptable use policy (AUP).",
-      "Understand ethical behavior as it relates to an AUP, Intellectual Property, Netiquette, Respecting Privacy, Anti- Spamming Laws, etc.",
+      {
+        standard: "Demonstrate knowledge of standard copyright rules.",
+        data: [
+          'Understand copyright for original creations.',
+          'Understand the creative commons license.',
+          'Understand when to obtain permission for non-original work.'
+        ]
+      },
+      {
+        standard: 'Identify the use and purpose of acceptable use policy (AUP).',
+        data: [
+          'Understand the AUP for your school.'
+        ]
+      },
+      {
+        standard: 'Understand ethical behavior as it relates to an AUP, Intellectual Property, Netiquette, Respecting Privacy, Anti- Spamming Laws, etc.',
+        data: []
+      }
     ],
     performance_skills: [
       "Understand copyright as it applies to you.",
@@ -61,10 +81,36 @@ export const cteLibrary: CTESkill[] = [
   },
   {
     id: "4",
-    strand: "Site Planning: Students will plan, design, implement, and maintain website(s). ",
+    strand:
+      "Site Planning: Students will plan, design, implement, and maintain website(s). ",
     standards: [
-      ''
+      "Analyze Project requirements.",
+      "Users are the focus of all design.",
+      "Code and design a minimum three-page website. (This is integrated into every Strand.)",
     ],
-    performance_skills: [],
+    performance_skills: [
+      "Build a mock-up, sketch, or wireframe of a website",
+      "Check for cross browser compatibility, readability, and accessibility",
+    ],
   },
+  {
+    id: "5",
+    strand:
+      "The student will create and prepare images to integrate with website designs, using image editing software.",
+    standards: [
+      "Open, edit, and save an image for effective use on your website.",
+      "Incorporate a logo and header into webpage.",
+      "Incorporate images into a website.",
+    ],
+    performance_skills: [
+      "Add an enhanced image to your website.",
+      "Optimize an image for quick loading on a website.",
+    ],
+  },
+  {
+    id: "6",
+    strand: 'Exploration & Preparation for Careers in Web Development: Students will explore careers in web development and prepare a portfolio of projects created.',
+    standards: [
+      'Explore IT web development careers.'
+  }
 ]
